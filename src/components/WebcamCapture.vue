e5d0f0485f53ff75339d5466fd74f3d4ef8ac404
sharique5 <hiresharique@gmail.com>
2020-09-01 08:58:52 +0530

    refactor helloworld component

---
+++ src/components/WebcamCapture.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+      <ul>
+          <li><video id="video" width="720" height="560" autoplay muted></video></li>
+          <li><button @click="takePic">Take Pic</button></li>
+          <li><canvas id="captured"></canvas></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import webCam from 'webcam-selfie-capture';
+
+export default {
+  name: 'WebcamCapture',
+  props: {},
+  data: function() {
+	return {
+		webCamInstance: null
+	}
+  },
+  mounted() {
+   let self = this
+    self.webCamInstance = webCam
+    self.webCamInstance.setVideoElementId('video')
+    self.webCamInstance.setVideoElementEventListener()
+    self.webCamInstance.loadModels()
+    self.webCamInstance.startVideo()
+  },
+  methods: {
+    takePic() {
+      let self = this
+      self.webCamInstance.captureSelfie('captured')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
